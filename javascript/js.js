cd4608842943a84dea4f8df8a67b1d042957bd60
Mauronns <mauronns44@gmail.com>
2024-11-12 15:50:35 -0300

    Add files via upload

---
+++ javascript/js.js
@@ -0,0 +1,6 @@
+function cambiarpagina() { document.body.style.backgroundImage = "none";
+  document.body.style.backgroundColor = "#4B0082";
+}
+window.onload = function() {alert("¡bienvenidos a mi pagina!");
+}
+function info() {alert("Hola, yo soy Mauro y estoy haciendo el curso de programacion web: front-end")}
